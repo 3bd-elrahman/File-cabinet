e7516fbde37b81c49b31bb0aabe57b869112150e
abdo <bodalala5@gmail.com>
2021-01-02 18:59:02 +0200

    adding doc controller

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -3,14 +3,34 @@ class DocsController < ApplicationController
   end
 
   def show
+    @doc = Doc.find(params[:id])
   end
 
   def edit
+  end
+  
+  def new 
+    @doc = Doc.new
+  end
+
+  def create
+   @doc = Doc.new(doc_params)
+   if @doc.save
+   redirect_to @doc
+   else
+   render 'new'
+   end
   end
 
   def update
   end
 
-  def delete
+  def destroy
+  end
+
+  private 
+
+  def doc_params
+  params.require(:doc).permit(:title,:content)
   end
 end
